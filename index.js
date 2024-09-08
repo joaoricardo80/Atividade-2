bef4b10c15b2c9bf2aa5b4a652e6ce90230a6a72
joao Ricardo <joao.ricardo.8@hotmail.com>
2024-09-08 04:05:59 -0300

    att

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import Cliente from "./Modelo/Cliente.js"; //nunca se esqueça da extensão .js
 
 const cliente = new Cliente("Rodeio", "18/10/2024", "Rua Beltrano, 123", "R$ 10,00");
 
-/*cliente.incluir().then(() =>{
+cliente.incluir().then(() =>{
     console.log("Cliente incluído com sucesso!");
 }).catch((erro) =>{
     console.log("Erro ao incluir o cliente: " +  erro);
-});*/
+});
 
 cliente.consultar("Rodeio").then((listaClientes)=>{
     for (const cliente of listaClientes){
